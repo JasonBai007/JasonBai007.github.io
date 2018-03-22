74ffe5a7f1e785dbe4cd7eb2eacfcb613e74d35f
JasonBai007 <byygkcg@163.com>
2018-03-22 16:30:47 +0800

    尝试解决兼容IOS

--- js/main.js
+++ js/main.js
@@ -1,96 +1,91 @@
-;(function () {
-	
-	'use strict';
+;(function() {
+  'use strict'
 
+  var isMobile = {
+    Android: function() {
+      return navigator.userAgent.match(/Android/i)
+    },
+    BlackBerry: function() {
+      return navigator.userAgent.match(/BlackBerry/i)
+    },
+    iOS: function() {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i)
+    },
+    Opera: function() {
+      return navigator.userAgent.match(/Opera Mini/i)
+    },
+    Windows: function() {
+      return navigator.userAgent.match(/IEMobile/i)
+    },
+    any: function() {
+      return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()
+    }
+  }
 
+  var getHeight = function() {
+    var extraHeight = 0
 
-	var isMobile = {
-		Android: function() {
-			return navigator.userAgent.match(/Android/i);
-		},
-			BlackBerry: function() {
-			return navigator.userAgent.match(/BlackBerry/i);
-		},
-			iOS: function() {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-			Opera: function() {
-			return navigator.userAgent.match(/Opera Mini/i);
-		},
-			Windows: function() {
-			return navigator.userAgent.match(/IEMobile/i);
-		},
-			any: function() {
-			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-		}
-	};
+    if (isMobile.any()) extraHeight = 50
 
-	var getHeight = function() {
-		var extraHeight = 0;
+    setTimeout(function() {
+      $('#fh5co-main')
+        .stop()
+        .animate({
+          height: $('.fh5co-tab-content.active').height() + extraHeight
+        })
+    }, 200)
+  }
 
-		if ( isMobile.any() ) extraHeight = 50;
-		
-		setTimeout(function(){
-			$('#fh5co-main').stop().animate({
-				'height': $('.fh5co-tab-content.active').height() + extraHeight
-			});
-		}, 200);
-	};
+  var pieChart = function() {
+    $('.chart').easyPieChart({
+      scaleColor: false,
+      lineWidth: 10,
+      lineCap: 'butt',
+      barColor: '#17e7a4',
+      trackColor: '#000000',
+      size: 160,
+      animate: 1000
+    })
+  }
 
-	var pieChart = function() {
-		$('.chart').easyPieChart({
-			scaleColor: false,
-			lineWidth: 10,
-			lineCap: 'butt',
-			barColor: '#17e7a4',
-			trackColor:	"#000000",
-			size: 160,
-			animate: 1000
-		});
-	};
+  var tabContainer = function() {
+    getHeight()
+    $(window).resize(function() {
+      getHeight()
+    })
+  }
 
-	var tabContainer = function() {
-		getHeight();
-		$(window).resize(function(){
-			getHeight();
-		})
-	};
+  var tabClickTrigger = function() {
+    $('.fh5co-tab-menu a').click(function(event) {
+      // $('.fh5co-tab-menu').on('click', 'a', function(event) {
+      // event.preventDefault();
+      var $this = $(this),
+        data = $this.data('tab'),
+        pie = $this.data('pie')
 
-	var tabClickTrigger = function() {
-		$('.fh5co-tab-menu a').click(function(event) {
-		// $('.fh5co-tab-menu').on('click', 'a', function(event) {
-			// event.preventDefault();
-			var $this = $(this),
-				data = $this.data('tab'),
-				pie = $this.data('pie');
+      // add/remove active class
+      $('.fh5co-tab-menu li').removeClass('active')
+      $this.closest('li').addClass('active')
 
-			// add/remove active class
-			$('.fh5co-tab-menu li').removeClass('active');
-			$this.closest('li').addClass('active');
+      $('.fh5co-tab-content.active').addClass('animated fadeOutDown')
 
-			$('.fh5co-tab-content.active').addClass('animated fadeOutDown');
+      setTimeout(function() {
+        $('.fh5co-tab-content.active').removeClass('active animated fadeOutDown fadeInUp')
+        $('.fh5co-tab-content[data-content="' + data + '"]').addClass('animated fadeInUp active')
+        getHeight()
+      }, 500)
 
-			setTimeout(function(){
-				$('.fh5co-tab-content.active').removeClass('active animated fadeOutDown fadeInUp');
-				$('.fh5co-tab-content[data-content="'+data+'"]').addClass('animated fadeInUp active');
-				getHeight();
-			}, 500);
+      if (pie === 'yes') {
+        setTimeout(function() {
+          pieChart()
+        }, 800)
+      }
+    })
+  }
 
-			if ( pie === 'yes' ) {
-				setTimeout(function(){
-					pieChart();
-				}, 800);
-			}
-			
-		})
-	};
-
-	// Document on load.
-	$(function(){
-		tabContainer();
-		tabClickTrigger();
-
-	});
-
-
-}());
+  // Document on load.
+  $(function() {
+    tabContainer()
+    tabClickTrigger()
+  })
+})()
