c60d72ffee1139f6780845802e68e973ebcd79ef
JasonBai007 <byygkcg@163.com>
2018-03-28 15:57:44 +0800

    升级到第二版

--- js/main.js
+++ js/main.js
@@ -1,118 +1,163 @@
-;(function() {
-  'use strict'
+'use strict'
 
-  var isMobile = {
-    Android: function() {
-      return navigator.userAgent.match(/Android/i)
-    },
-    BlackBerry: function() {
-      return navigator.userAgent.match(/BlackBerry/i)
-    },
-    iOS: function() {
-      return navigator.userAgent.match(/iPhone|iPad|iPod/i)
-    },
-    Opera: function() {
-      return navigator.userAgent.match(/Opera Mini/i)
-    },
-    Windows: function() {
-      return navigator.userAgent.match(/IEMobile/i)
-    },
-    any: function() {
-      return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()
-    }
-  }
-  var getHeight = function() {
-    var extraHeight = 0
+$(window).on('load', function() {
+  /*------------------
+		Preloder
+	--------------------*/
+  $('.loader').fadeOut()
+  $('#preloder')
+    .delay(400)
+    .fadeOut('slow')
 
-    if (isMobile.any()) extraHeight = 50
+  /*------------------
+		Isotope Filter
+	--------------------*/
+  var $container = $('.isotope_items')
+  $container.isotope()
 
-    setTimeout(function() {
-      $('#fh5co-main')
-        .stop()
-        .animate({
-          height: $('.fh5co-tab-content.active').height() + extraHeight
-        })
-    }, 200)
-  }
-
-  var pieChart = function() {
-    $('.chart').easyPieChart({
-      scaleColor: false,
-      lineWidth: 10,
-      lineCap: 'butt',
-      barColor: '#17e7a4',
-      trackColor: '#000000',
-      size: 160,
-      animate: 1000
+  $('.portfolio-filter li').on('click', function() {
+    $('.portfolio-filter li').removeClass('active')
+    $(this).addClass('active')
+    var selector = $(this).attr('data-filter')
+    $('.isotope_items').isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 750,
+        easing: 'linear',
+        queue: false
+      }
     })
-  }
-
-  var tabContainer = function() {
-    getHeight()
-    $(window).resize(function() {
-      getHeight()
-    })
-  }
-
-  var tabClickTrigger = function() {
-    if (window.orientation !== undefined) {
-      $('.fh5co-tab-menu li').mouseenter(function(event) {
-        // $('.fh5co-tab-menu').on('click', 'a', function(event) {
-        // event.preventDefault();
-        var $this = $(this).children('a'),
-          data = $this.data('tab'),
-          pie = $this.data('pie')
-
-        // add/remove active class
-        $('.fh5co-tab-menu li').removeClass('active')
-        $this.closest('li').addClass('active')
-
-        $('.fh5co-tab-content.active').addClass('animated fadeOutDown')
-
-        setTimeout(function() {
-          $('.fh5co-tab-content.active').removeClass('active animated fadeOutDown fadeInUp')
-          $('.fh5co-tab-content[data-content="' + data + '"]').addClass('animated fadeInUp active')
-          getHeight()
-        }, 500)
-
-        if (pie === 'yes') {
-          setTimeout(function() {
-            pieChart()
-          }, 800)
-        }
-      })
-    } else {
-      $('.fh5co-tab-menu li').click(function(event) {
-        // $('.fh5co-tab-menu').on('click', 'a', function(event) {
-        // event.preventDefault();
-        var $this = $(this).children('a'),
-          data = $this.data('tab'),
-          pie = $this.data('pie')
-
-        // add/remove active class
-        $('.fh5co-tab-menu li').removeClass('active')
-        $this.closest('li').addClass('active')
-
-        $('.fh5co-tab-content.active').addClass('animated fadeOutDown')
-
-        setTimeout(function() {
-          $('.fh5co-tab-content.active').removeClass('active animated fadeOutDown fadeInUp')
-          $('.fh5co-tab-content[data-content="' + data + '"]').addClass('animated fadeInUp active')
-          getHeight()
-        }, 500)
-
-        if (pie === 'yes') {
-          setTimeout(function() {
-            pieChart()
-          }, 800)
-        }
-      })
-    }
-  }
-
-  // Document on load.
-  $(function() {
-    tabContainer()
-    tabClickTrigger()
+    return false
   })
-})()
+})
+
+;(function($) {
+  /*------------------
+  		HEADER
+  	--------------------*/
+  var navMenu = $('.menu-list')
+  navMenu.onePageNav()
+  $(window).on('scroll resize', function(e) {
+    if ($(this).scrollTop() > 70) {
+      $('.header-section').addClass('sticky')
+    } else {
+      $('.header-section').removeClass('sticky')
+    }
+    e.preventDefault()
+  })
+
+  $('.responsive').on('click', function(event) {
+    $('.menu-list').slideToggle(400)
+    $('.header-section').toggleClass('bgc')
+    event.preventDefault()
+  })
+
+  $('.menu-list li a').on('click', function(event) {
+    if ($(window).width() < 768) {
+      $('.menu-list').slideUp(400)
+      $('.header-section').removeClass('bgc')
+    }
+  })
+
+  /*------------------
+		TYPED JS
+	--------------------*/
+  $('.element').typed({
+    strings: ["I'm Jason Bai", 'Web Developer', 'Photographer', 'From Beijing'],
+    typeSpeed: 10,
+    loop: true,
+    backDelay: 2000
+  })
+
+  /*------------------
+		FOOTER
+	--------------------*/
+  var fh = $('.footer-section').height()
+  fh = fh + 140
+  $('.main-warp').css('margin-bottom', fh)
+
+  /*------------------
+		PROGRESS BAR
+	--------------------*/
+  $('.progress-bar-style').each(function() {
+    var progress = $(this).data('progress')
+    var prog_width = progress + '%'
+    if (progress <= 100) {
+      $(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>')
+    } else {
+      $(this).append('<div class="bar-inner" style="width:100%"><span>100%</span></div>')
+    }
+  })
+
+  /*------------------
+		OWL CAROUSEL
+	--------------------*/
+  $('#review-carousel').owlCarousel({
+    dots: false,
+    nav: true,
+    loop: true,
+    margin: 30,
+    smartSpeed: 700,
+    items: 1,
+    autoplay: true,
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+  })
+
+  /*------------------
+		MAGNIDIC POPUP
+	--------------------*/
+  $('.work-item').magnificPopup({
+    type: 'image',
+    gallery: { enabled: true },
+    removalDelay: 400,
+    zoom: { enabled: true, duration: 300 }
+  })
+
+  /*------------------
+		WOW JS
+	--------------------*/
+  new WOW().init()
+
+  /*------------------
+		CONTACT FORM
+	--------------------*/
+  $('#contact-form').on('submit', function() {
+    var send_btn = $('#send-form'),
+      form = $(this),
+      formdata = $(this).serialize(),
+      chack = $('#form-chack')
+    send_btn.text('Wait...')
+
+    function reset_form() {
+      $('#name').val('')
+      $('#email').val('')
+      $('#massage').val('')
+    }
+
+    $.ajax({
+      url: $(form).attr('action'),
+      type: 'POST',
+      data: formdata,
+      success: function(text) {
+        if (text == 'success') {
+          send_btn.addClass('done')
+          send_btn.text('Success')
+          setTimeout(function() {
+            reset_form()
+            send_btn.removeClass('done')
+            send_btn.text('Massage')
+          }, 2500)
+        } else {
+          reset_form()
+          send_btn.addClass('error')
+          send_btn.text('Error')
+          setTimeout(function() {
+            send_btn.removeClass('error')
+            send_btn.text('Massage')
+          }, 5000)
+        }
+      }
+    })
+    return false
+  })
+})(jQuery)
